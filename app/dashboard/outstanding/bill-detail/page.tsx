2ac38b79392ba2a0c6d88798a6f7e54e56723e21
Jayesh Mudliyar <mudliyar_jayesh@Jayeshs-Mac-mini.local>
2024-07-23 14:50:17 +0530

    Outstanding API Endpoints Integrated

--- app/dashboard/outstanding/bill-detail/page.tsx
+++ app/dashboard/outstanding/bill-detail/page.tsx
@@ -1,6 +1,142 @@
+"use client";
+import { getBmrmBaseUrl, postAsync } from "@/app/services/rest_services";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+
 const Page = () => {
+
+  const partyName: string = localStorage.getItem("bill_party_name") || "";
+  const filterValue = localStorage.getItem("party_filter_value");
+  const viewType = localStorage.getItem("party_view_type");
+  const billType = localStorage.getItem("party_bill_type"); 
+  const filterType = localStorage.getItem("party_filter_type") || null; 
+
+  useEffect(() => {
+    onApi();
+  }, [])
+
+  const [rows, setRows] = useState([])
+  const columns: GridColDef<any[number]>[] = [
+    {
+      field: "billDate",
+      headerName: "Date",
+      editable: false,
+      sortable: true,
+      minWidth: 50,
+      maxWidth: 400,
+    },
+    {
+      field: "billNumber",
+      headerName: "Bill No.",
+      editable: false,
+      sortable: true,
+      minWidth: 50,
+      maxWidth: 400,
+    },
+    {
+      field: "amount",
+      headerName: "Value",
+      editable: false,
+      sortable: true,
+      minWidth: 50,
+      maxWidth: 400,
+      valueGetter: (value, row) => `${row.currency || ""} ${row.amount || "0"}`,
+    },
+    {
+      field: "dueDate",
+      headerName: "Due Date",
+      editable: false,
+      sortable: true,
+      minWidth: 50,
+      maxWidth: 400,
+    },
+    {
+      field: "breachDays",
+      headerName: "Delay Days",
+      editable: false,
+      sortable: true,
+      minWidth: 50,
+      maxWidth: 400,
+    },
+  ];
+
+
+
+  const onApi = async () => {
+    let party = partyName.replace("&", "%26");
+    let collectionUrl = `${getBmrmBaseUrl()}/bill/get/upcoming-bill-detail`;
+    let agingUrl = `${getBmrmBaseUrl()}/bill/get/aging-bill-detail?agingCode=${filterValue}&groupType=${billType}&partyName=${party}`;
+    let totalOutstandingUrl = `${getBmrmBaseUrl()}/bill/get/party-bill-detail?groupType=${billType}&partyName=${party}`;
+
+    let url = totalOutstandingUrl;
+    if (viewType === "upcoming") {
+      url = collectionUrl;
+    } else if (viewType === "aging") {
+      url = agingUrl;
+    }
+    let requestBody;
+    if (viewType === "upcoming") {
+      requestBody = {
+        groupType: billType,
+        durationType: filterType,
+        durationKey: filterValue,
+        partyName: partyName,
+        filter: {
+          page_number: 0,
+          page_size: 20,
+          search_text: "",
+          sort_by: "billNumber",
+          sort_order: "asc",
+        },
+      };
+    } else {
+      requestBody = {
+        page_number: 0,
+        page_size: 20,
+        search_text: "",
+        sort_by: "billNumber",
+        sort_order: "",
+      };
+    }
+    let response = await postAsync(url, requestBody);
+    
+    let entries = response.map((entry: any, index: number) => {
+      return {
+        id: index + 1,//entry.bill_id,
+        billNumber: entry.billNumber,
+        amount: entry.totalAmount,
+        dueDate: entry.dueDate,
+        breachDays: entry.breachDays,
+        billDate: entry.billDate,
+        currency: entry.currency ?? "₹",
+      };
+    });
+
+    //  console.log(`requestBody Party bill Detail : ${JSON.stringify(entries,  0 , index =2 )}`)
+
+    setRows(entries);
+
+    return entries;
+  };
+
   return (<div>
     <h3>Bill Detail</h3>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {
+          alert(`page model:  ${JSON.stringify(value)}`);
+        }}
+      />
   </div>);
 }
 
--- app/dashboard/outstanding/page.tsx
+++ app/dashboard/outstanding/page.tsx
@@ -3,11 +3,144 @@
 import { getAsync, getBmrmBaseUrl } from "@/app/services/rest_services";
 import { ListView } from "@/app/ui/list_view";
 import { useEffect, useRef, useState } from "react";
-import { Card, CardContent, CardMedia, Typography, Box, FormControl, FormHelperText, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Container,
+  Typography,
+  Box,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Button,
+} from "@mui/material";
 import { useRouter } from "next/navigation";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { DropDown } from "@/app/ui/drop_down";
 import { League_Spartan } from "next/font/google";
+import { PieChart } from "@mui/x-charts";
+import { endianness } from "os";
+import { inspiredPalette } from "@/app/ui/theme";
+import { setFips } from "crypto";
+
+interface CustomCardProps {
+  data: CardData;
+  onPress: (entry: CardData) => void;
+}
+const CustomCard: React.FC<CustomCardProps> = ({ data, onPress }) => {
+  return (
+    <Card
+      sx={{ minWidth: 200, margin: "0 8px" }}
+      onClick={(event) => {
+        onPress(data);
+      }}
+    >
+      <CardContent>
+        <Typography variant="h5" component="div">
+          {data.title}
+        </Typography>
+        <Typography variant="body2">{data.content}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+interface CardData {
+  title: string;
+  content: string;
+  code: string;
+}
+
+const Home = ({ billType }: { billType: string }) => {
+  const [data, setData] = useState<CardData[]>([]);
+  const router = useRouter();
+  useEffect(() => {
+    loadData();
+  }, [billType]);
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/bill/get/aging-overview?groupType=${billType}`;
+      let response = await getAsync(url);
+      let entries = response.map((entry: any, index: number) => {
+        let newEntry: CardData = {
+          title: entry.title,
+          code: entry.code,
+          content: `${entry.amount}`,
+        };
+        return newEntry;
+      });
+      let agingDetails = entries.sort((a: any, b: any) => {
+        const name1 = a.title.toLowerCase();
+        const name2 = b.title.toLowerCase();
+
+        if (name1 < name2) {
+          return -1;
+        }
+        if (name1 > name2) {
+          return 1;
+        }
+        return 0;
+      });
+      setData(agingDetails);
+    } catch {
+      alert("Could not load data");
+    }
+  };
+
+  return (
+    <Container>
+      <Box sx={{ display: "flex", overflowX: "auto", padding: "16px 0" }}>
+        {data.map((card, index) => (
+          <CustomCard
+            key={index}
+            data={card}
+            onPress={(data) => {
+              localStorage.setItem("party_filter_value", data.code);
+              localStorage.setItem("party_view_type", "aging");
+              localStorage.setItem("party_bill_type", billType);
+              router.push("/dashboard/outstanding/party-search");
+            }}
+          />
+        ))}
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <PieChart
+          width={400}
+          height={500}
+          slotProps={{
+            legend: {
+              position: {
+                horizontal: "left",
+                vertical: "bottom",
+              },
+            },
+          }}
+          series={[
+            {
+              data: data.map((entry: CardData) => {
+                return {
+                  label: entry.title,
+                  value: parseFloat(entry.content),
+                };
+              }),
+              innerRadius: 30,
+              outerRadius: 100,
+              paddingAngle: 5,
+              cornerRadius: 5,
+              startAngle: 0,
+              endAngle: 360,
+              cx: 150,
+              cy: 150,
+            },
+          ]}
+        />
+      </Box>
+    </Container>
+  );
+};
 
 const Page = () => {
   const router = useRouter();
@@ -39,8 +172,8 @@ const Page = () => {
   const loadUpcoming = async () => {
     try {
       let url = `${getBmrmBaseUrl()}/bill/get/upcoming-overview?groupType=${
-        selectedType.current
-      }&durationType=daily`;
+        selectedType.current.code
+      }&durationType=${selectedFilter.current.value}`;
       let response = await getAsync(url);
       let entries = response.map((entry: any) => {
         return {
@@ -81,10 +214,20 @@ const Page = () => {
     <div>
       <h3>Outstanding Dashboard</h3>
 
-      <DropDown 
+      <Button variant="contained" onClick={(event) => {
+          localStorage.setItem("party_filter_value", "");
+          localStorage.setItem("party_view_type", "all_parties");
+          localStorage.setItem("party_bill_type", selectedType.current.code);
+          localStorage.setItem("party_filter_type", "");
+          router.push("/dashboard/outstanding/party-search");
+
+      }}>View All Parties</Button>
+      <br/>
+
+      <DropDown
         label="Select Type"
         displayFieldKey={"label"}
-        valueFieldKey={"code"}
+        valueFieldKey={null}
         selectionValues={types}
         helperText={"Select Outstanding Type"}
         onSelection={(selection) => {
@@ -93,23 +236,63 @@ const Page = () => {
         }}
       />
 
-      <DataGrid
-      columns={columns}
-      rows={rows}
-      initialState={{
-        pagination: {
-          paginationModel: {
-            pageSize: 10
-          }
-        }
-      }}
-      pageSizeOptions={[5, 10, 25, 50, 75, 100]}
-      disableRowSelectionOnClick
-      onPaginationModelChange={(value) => {
-        alert(`page model:  ${JSON.stringify(value)}`);
-      }}
-      />
+      <Home billType={selectedType.current.code} />
+      <Container>
+        <Box sx={{ display: "flex", overflowX: "auto", padding: "16px 0" }}>
+          {filters.map((card, index) => (
+            <Card
+              key={index}
+              sx={{
+                minWidth: 200,
+                margin: "0 8px",
+                background: card.isSelected ? inspiredPalette.Pumpkin : "white",
+                color: card.isSelected ? "white" : "black",
+              }}
+              onClick={(event) => {
+                let values: any[] = filters;
+                values = values.map((entry: any) => {
+                  let isSelected = card.value === entry.value;
+                  entry.isSelected = isSelected;
+                  return entry;
+                });
+                updateFilters(values);
+                selectedFilter.current = card;
+                loadUpcoming();
+              }}
+            >
+              <CardContent>
+                <Typography variant="h5" component="div">
+                  {card.label}
+                </Typography>
+              </CardContent>
+            </Card>
+          ))}
+        </Box>
+      </Container>
 
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        onRowClick={(params) => {
+          localStorage.setItem("party_filter_value", params.row.id);
+          localStorage.setItem("party_view_type", "upcoming");
+          localStorage.setItem("party_bill_type", selectedType.current.code);
+          localStorage.setItem("party_filter_type", selectedFilter.current.value);
+          router.push("/dashboard/outstanding/party-search");
+        }}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {
+          alert(`page model:  ${JSON.stringify(value)}`);
+        }}
+      />
     </div>
   );
 };
