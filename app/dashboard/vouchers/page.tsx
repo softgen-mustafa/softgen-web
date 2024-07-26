303a0797c6b93903643f362e827d87d62249f044
Jayesh Mudliyar <mudliyar_jayesh@Jayeshs-Mac-mini.local>
2024-07-26 15:23:51 +0530

    Voucher Reports

---
+++ app/dashboard/vouchers/page.tsx
@@ -0,0 +1,404 @@
+"use client";
+import {
+  getAsync,
+  getBmrmBaseUrl,
+  postAsync,
+} from "@/app/services/rest_services";
+import { DataTable } from "@/app/ui/data_grid";
+import { DropDown } from "@/app/ui/drop_down";
+import { CardView, GridConfig, RenderGrid } from "@/app/ui/responsive_grid";
+import { ChevronLeftRounded, LabelOffRounded } from "@mui/icons-material";
+import { Grid, IconButton, Typography } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+
+const ItemGroupCard = ({ voucherType }: { voucherType: string }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [voucherType]);
+
+  const columns: GridColDef[] = [
+    {
+      field: "itemGroup",
+      headerName: "Stock Group",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "amount",
+      headerName: "Value",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "quantity",
+      headerName: "Qty",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+  ];
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/meta-voucher/item-group/overview?voucherType=${voucherType}`;
+      let response = await postAsync(url, {});
+      setData(response.map((entry: any) => {
+        return {
+          id: entry.itemGroup,
+          ...entry
+        }
+      }));
+    } catch {
+      alert("Could not load data");
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <DataGrid
+        columns={columns}
+        rows={data}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        onRowClick={(params) => {}}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {}}
+      />
+    </div>
+  );
+};
+
+
+const BillsCard = ({ voucherType }: { voucherType: string }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [voucherType]);
+
+  const columns: GridColDef[] = [
+    {
+      field: "voucherNumber",
+      headerName: "Bill",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "partyName",
+      headerName: "Party",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "preGstAmount",
+      headerName: "Value",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "postGstAmount",
+      headerName: "Post Tax Value",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+  ];
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/meta-voucher/bill/overview?voucherType=${voucherType}`;
+      let response = await postAsync(url, {});
+      setData(response.map((entry: any) => {
+        return {
+          id: entry.voucherNumber,
+          ...entry
+        }
+      }));
+    } catch {
+      alert("Could not load data");
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <DataGrid
+        columns={columns}
+        rows={data}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        onRowClick={(params) => {}}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {}}
+      />
+    </div>
+  );
+};
+
+
+const MonthlySalesCard = ({ voucherType }: { voucherType: string }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [voucherType]);
+
+  const columns: GridColDef[] = [
+    {
+      field: "monthStr",
+      headerName: "Month",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "preGstAmount",
+      headerName: "Value",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+  ];
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/meta-voucher/monthly/overview?voucherType=${voucherType}`;
+      let response = await postAsync(url, {});
+      console.log(`response: ${JSON.stringify(response)}`)
+      setData(response.map((entry: any) => {
+        return {
+          id: entry.monthStr,
+          monthStr: entry.monthStr,
+          preGstAmount: entry.preGstAmount,
+        }
+      }));
+    } catch {
+      alert("Could not load data");
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <DataGrid
+        columns={columns}
+        rows={data}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        onRowClick={(params) => {}}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {}}
+      />
+    </div>
+  );
+};
+
+const MonthlyCustomerSalesCard = ({ voucherType }: { voucherType: string }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, [voucherType]);
+
+  const columns: GridColDef[] = [
+    {
+      field: "partyName",
+      headerName: "Party",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "monthStr",
+      headerName: "Month",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+    {
+      field: "preGstAmount",
+      headerName: "Value",
+      editable: false,
+      sortable: true,
+      minWidth: 300,
+      maxWidth: 400,
+    },
+  ];
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/meta-voucher/monthly/customer/overview?voucherType=${voucherType}`;
+      let response = await postAsync(url, {});
+      setData(response.map((entry: any) => {
+        return {
+          id: entry.partyName,
+          partyName: entry.partyName,
+          monthStr: entry.monthStr,
+          preGstAmount: entry.preGstAmount
+        }
+      }));
+    } catch {
+      alert("Could not load data");
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <DataGrid
+        columns={columns}
+        rows={data}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 10,
+            },
+          },
+        }}
+        onRowClick={(params) => {}}
+        pageSizeOptions={[5, 10, 25, 50, 75, 100]}
+        disableRowSelectionOnClick
+        onPaginationModelChange={(value) => {}}
+      />
+    </div>
+  );
+};
+
+const Page = () => {
+  const router = useRouter();
+
+  const [voucherTypes, setVoucherTypes] = useState([]);
+  const [selectedVoucherType, setVoucherType] = useState("Sales");
+
+  useEffect(() => {
+    loadVoucherTypes();
+  }, []);
+
+  const loadVoucherTypes = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/meta-voucher/get/voucher-types`;
+      let response = await getAsync(url);
+      setVoucherTypes(
+        response.map((entry: any) => {
+          return {
+            id: entry.name,
+            name: entry.name,
+          };
+        })
+      );
+    } catch {
+      alert("Could not load voucher types");
+    }
+  };
+
+  const gridConfig: GridConfig[] = [
+    {
+      type: "item",
+      className: "",
+      view: (
+        <CardView>
+          <div className="flex flex-row items-center">
+            <IconButton
+              onClick={() => {
+                router.back();
+              }}
+            >
+              <ChevronLeftRounded />
+            </IconButton>
+            <Typography>Go Back</Typography>
+          </div>
+          <br />
+          <DropDown
+            label="Select Type"
+            displayFieldKey={"name"}
+            valueFieldKey={null}
+            selectionValues={voucherTypes}
+            helperText={"Select Outstanding Type"}
+            onSelection={(selection) => {
+              setVoucherType(selection.name);
+            }}
+          />
+          <MonthlySalesCard voucherType={selectedVoucherType}/>
+        </CardView>
+      ),
+      children: [],
+    },
+    {
+      type: "item",
+      className: "",
+      view: (
+        <CardView>
+          <MonthlyCustomerSalesCard voucherType={selectedVoucherType}/>
+        </CardView>
+      ),
+      children: [],
+    },
+    {
+      type: "item",
+      className: "",
+      view: (
+        <CardView>
+          <ItemGroupCard voucherType={selectedVoucherType}/>
+        </CardView>
+      ),
+      children: [],
+    },
+    {
+      type: "item",
+      className: "",
+      view: (
+        <CardView>
+          <BillsCard voucherType={selectedVoucherType}/>
+        </CardView>
+      ),
+      children: [],
+    },
+  ];
+
+  return (
+    <div className="w-full" style={{}}>
+      <Grid
+        container
+        className="bg-gray-200"
+        sx={{
+          flexGrow: 1,
+          height: "100vh",
+        }}
+      >
+        {RenderGrid(gridConfig)}
+      </Grid>
+    </div>
+  );
+};
+export default Page;
