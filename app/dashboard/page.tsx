3a81c0a02eb758b3aba6b2653bddd876699b8eb2
Jayesh Mudliyar <mudliyar_jayesh@Jayeshs-Mac-mini.local>
2024-07-29 15:00:39 +0530

    Company Selection

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,9 +6,64 @@ import { SalesReportCard } from "./cards/sales_report_card";
 import { InventoryCard } from "./cards/inventory_card";
 import { OutstandingTask } from "./cards/outstanding_task_card";
 import { CardView, GridConfig, RenderGrid } from "../ui/responsive_grid";
+import { DropDown } from "../ui/drop_down";
+import { getAsync, getBmrmBaseUrl } from "../services/rest_services";
+import { useEffect, useRef, useState } from "react";
+import Cookies from "js-cookie";
+
+const CompanyCard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    try {
+      let url = `${getBmrmBaseUrl()}/info/user-tenant/get/companies`;
+      let response = await getAsync(url);
+      let values = response.map((entry: any) => {
+        return {
+          id: entry["company_id"],
+          name: entry["company_name"],
+          user_id: entry.user_id,
+        };
+      });
+      setData(values);
+      if (values && values.length > 0) {
+        Cookies.set("companyId", values[0].id);
+      }
+    } catch {}
+  };
+
+  return (
+    <div>
+      <DropDown
+        label={"Select Company"}
+        displayFieldKey={"name"}
+        valueFieldKey={null}
+        selectionValues={data}
+        helperText={""}
+        onSelection={(selection) => {
+          Cookies.set("companyId", selection.id);
+        }}
+      />
+    </div>
+  );
+};
 
 const DashboardPage = () => {
   const gridConfig: GridConfig[] = [
+    {
+      type: "item",
+      view: (
+        <CardView>
+          <CompanyCard />
+        </CardView>
+      ),
+      className: "",
+      children: [],
+    },
     {
       type: "item",
       view: (
@@ -59,8 +114,6 @@ const DashboardPage = () => {
       className: "",
       children: [],
     },
-    
-
   ];
   return (
     <div className="">
