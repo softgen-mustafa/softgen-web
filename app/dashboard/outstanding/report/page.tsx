51f3534381373edfde35c6679402dbbd6106955b
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-08-25 17:22:32 +0530

    Group Filter in os report

--- app/dashboard/outstanding/report/page.tsx
+++ app/dashboard/outstanding/report/page.tsx
@@ -1,16 +1,41 @@
 "use client";
 
 import { numericToString } from "@/app/services/Local/helper";
+import { DropDown } from "@/app/ui/drop_down";
 import { DataTable } from "@/app/ui/data_grid";
 import { CardView, DynGrid, Weight, GridDirection } from "@/app/ui/responsive_grid";
 import { GridColDef } from "@mui/x-data-grid";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 
 const Page = () => {
 
     const [refresh, setRefresh] = useState(false);
+    const [groups, setGroups] = useState([]);
+
+    let selectedGroups = useRef<string[]>([])
+
+    useEffect(() => {
+        loadGroups().then(_ => setRefresh(!refresh))
+    }, [])
+
+    const loadGroups = async () => {
+        let url = "http://118.139.167.125:45700/os/get/groups?isDebit=true";
+        let appHeaders = {
+            "Content-Type": "application/json; charset=utf-8",
+            "CompanyId": Cookies.get("companyId") ?? 1,
+        };
+        let res = await axios.get(url,{ headers: appHeaders })
+        let values = res.data.map((entry: string) => {
+            return {
+                name: entry,
+                value: entry
+            }
+        })
+        setGroups(values);
+
+    }
 
     const loadData = async (offset: number, limit: number, search?: string) => {
         let url = "http://118.139.167.125:45700/os/get/report?isDebit=true";
@@ -19,7 +44,7 @@ const Page = () => {
             "Offset": offset,
             "PartyName": "",
             "SearchText": search,
-            "Groups": [],
+            "Groups": selectedGroups.current ?? [],
             "DueDays": 30,
             "OverDueDays": 90
         }
@@ -108,6 +133,28 @@ const Page = () => {
     ];
 
     const gridConfig = [
+        {
+        weight: Weight.Low,
+        view: (
+            <CardView title="Outstanding Overview">
+            <DropDown
+            label="Select Type"
+            displayFieldKey={"name"}
+            valueFieldKey={null}
+            selectionValues={groups}
+            helperText={""}
+            onSelection={(selection: any) => {
+                /*let exists = selectedGroups.current.indexOf(selection.name);
+                if (exists === -1) {
+                    selectedGroups.current.push(selection.name)
+                }*/
+                selectedGroups.current = [selection.name];
+                setRefresh(!refresh);
+            }}
+            />
+            </CardView>
+        )
+        },
         {
             weight: Weight.High,
             view: (
