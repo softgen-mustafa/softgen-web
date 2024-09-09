4cba9b494369871b376f8d1986522f81ee6f3fc9
Khan Mustafa <146811268+softgen-mustafa@users.noreply.github.com>
2024-09-09 14:51:22 +0530

    Theme Update

--- app/theme.tsx
+++ app/theme.tsx
@@ -4,6 +4,88 @@ import "@fontsource/poppins";
 import { Theme, createTheme } from "@mui/material";
 import { inspiredPalette } from "./ui/theme";
 
+// const appThemes: any = [
+//   {
+//     name: "Light",
+//     code: "light",
+//     theme: createTheme({
+//       typography: {
+//         fontFamily: "Poppins, sans-serif",
+//       },
+//       palette: {
+//         primary: {
+//           main: "#303f9f",
+//           light: "#1d265c",
+//           contrastText: "#ffffff",
+//         },
+//         secondary: {
+//           main: "#FFFFFF",
+//           contrastText: "#1EAFE5",
+//         },
+//       },
+//     }),
+//   },
+//   {
+//     name: "Dark",
+//     code: "dark",
+//     theme: createTheme({
+//       typography: {
+//         fontFamily: "Poppins, sans-serif",
+//       },
+//       palette: {
+//         primary: {
+//           main: inspiredPalette.dark,
+//           contrastText: "#ffffff",
+//         },
+//         secondary: {
+//           main: "#FFFFFF",
+//           contrastText: "#1EAFE5",
+//         },
+//       },
+//     }),
+//   },
+//   {
+//     name: "Evening",
+//     code: "evening",
+//     theme: createTheme({
+//       typography: {
+//         fontFamily: "Poppins, sans-serif",
+//       },
+//       palette: {
+//         primary: {
+//           main: inspiredPalette.lightOrange,
+//           light: "#f2815e",
+//           contrastText: "#232325",
+//         },
+//         secondary: {
+//           main: "#FFFFFF",
+//           contrastText: "#1EAFE5",
+//         },
+//       },
+//     }),
+//   },
+//   {
+//     name: "Morning",
+//     code: "morning",
+//     theme: createTheme({
+//       typography: {
+//         fontFamily: "Poppins, sans-serif",
+//       },
+//       palette: {
+//         primary: {
+//           main: inspiredPalette.lightBlue,
+//           light: "#5b9dd4",
+//           contrastText: "#232325",
+//         },
+//         secondary: {
+//           main: "#FFFFFF",
+//           contrastText: "#1EAFE5",
+//         },
+//       },
+//     }),
+//   },
+// ];
+
 const appThemes: any = [
   {
     name: "Light",
@@ -14,8 +96,8 @@ const appThemes: any = [
       },
       palette: {
         primary: {
-          main: "#303f9f",
-          light: "#1d265c",
+          main: "#8c57ff",
+          light: "#7841ef",
           contrastText: "#ffffff",
         },
         secondary: {
@@ -34,7 +116,8 @@ const appThemes: any = [
       },
       palette: {
         primary: {
-          main: inspiredPalette.dark,
+          main: "#0d9394",
+          // light: "",
           contrastText: "#ffffff",
         },
         secondary: {
@@ -53,9 +136,9 @@ const appThemes: any = [
       },
       palette: {
         primary: {
-          main: inspiredPalette.lightOrange,
-          light: "#f2815e",
-          contrastText: "#232325",
+          main: "#ffab1d",
+          // light: "#f2815e",
+          contrastText: "#000000de",
         },
         secondary: {
           main: "#FFFFFF",
@@ -73,9 +156,9 @@ const appThemes: any = [
       },
       palette: {
         primary: {
-          main: inspiredPalette.lightBlue,
-          light: "#5b9dd4",
-          contrastText: "#232325",
+          main: "#eb3d63",
+          // light: "#5b9dd4",
+          contrastText: "#FFFFFF",
         },
         secondary: {
           main: "#FFFFFF",
