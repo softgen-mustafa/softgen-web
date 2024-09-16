99dac1f3657e45937addd60f02e5e583afb0c4ca
Mudliyar Jayesh <mudliyar.jayesh@gmail.com>
2024-09-16 20:33:39 +0530

    follow up creation navigation

--- app/dashboard/followup/page.tsx
+++ app/dashboard/followup/page.tsx
@@ -5,9 +5,12 @@ import ResponsiveCardGrid from "@/app/components/ResponsiveCardGrid"
 import {DateWiseFollowup }from "./datewisefollowup";
 import {TeamFollowup} from "./teamfollowup";
 import {PartyFollowup} from "./partyfollowup";
-import { Typography } from "@mui/material"
+import { Typography, Box } from "@mui/material"
+import { Create } from "@mui/icons-material";
+import { useRouter } from "next/navigation"
 
 const Page = () => {
+    const router = useRouter();
     const gridConfig: any[] = [
         {
             id: 1,
@@ -49,6 +52,23 @@ const Page = () => {
 
     return (
         <div className="w-full ">
+      <Box
+        width={50}
+        height={50}
+        borderRadius={2}
+        border={2}
+        borderColor="#000000"
+        className="flex items-center shadow-lg justify-center fixed mr-3 mb-2 bg-white"
+        sx={{
+            bottom: 0,
+            right: 0,
+        }}
+        onClick={() => {
+            router.push("/dashboard/followup/create");
+        }}
+      >
+        <Create sx={{ color: "#000000" }} />
+      </Box>
         <ResponsiveCardGrid screenName="FollowUpDashboard" initialCards={gridConfig}/>
         </div>
     );
