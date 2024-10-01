1b26a179b6aea9412ad774dd2b9243f7c4df38eb
aquib-softgen <softgen.aquib.shaikh@gmail.com>
2024-10-01 16:19:49 +0530

    Email selection resolved

--- app/dashboard/outstanding/report/outstanding_setings.tsx
+++ app/dashboard/outstanding/report/outstanding_setings.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/app/services/rest_services";
 import { useSnackbar } from "@/app/ui/snack_bar_provider";
 import { useRouter } from "next/navigation";
+import { ApiDropDown } from "@/app/ui/api_drop_down";
 
 interface EmailSettings {
   SmtpServer: string;
@@ -36,6 +37,7 @@ interface OsSettings {
   SendDueOnly: boolean;
   AutoReminderInterval: number;
   ReminderIntervalDays: number;
+  TemplateName: string;
   EmailSetting: EmailSettings;
 }
 
@@ -50,6 +52,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
     SendDueOnly: false,
     AutoReminderInterval: 0,
     ReminderIntervalDays: 10,
+    TemplateName: "",
     EmailSetting: {
       SmtpServer: "",
       SmtpPort: "",
@@ -65,6 +68,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
 
   const [settings, setSettings] = useState<OsSettings>(initialSettings);
   const [showEmailConfig, toggleEmailConfig] = useState(false);
+  const [selectedTemplate, setSelectedTemplate] = useState<string>("");
   const isSettingsLoaded = useRef(false);
   const router = useRouter();
 
@@ -74,13 +78,17 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
     const loadData = async () => {
       try {
         let url = `${getSgBizBaseUrl()}/os-setting/get`;
+
+        console.log("--loadData--", url);
         let response = await getAsync(url);
+
         if (response.Data && response.Data.length > 0) {
           setSettings(response.Data[0]);
           isSettingsLoaded.current = true;
         } else {
           isSettingsLoaded.current = false;
         }
+        console.log(JSON.stringify(response));
       } catch (error) {
         console.error("Error loading data:", error);
         isSettingsLoaded.current = false;
@@ -94,15 +102,26 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
       let url = `${getSgBizBaseUrl()}/os-setting/create`;
       console.log("Create URL hit:", url);
 
-      let requestBody = { ...settings };
+      let requestBody = {
+        ...settings,
+        TemplateName: selectedTemplate || settings.TemplateName,
+      };
+
+      if (!selectedTemplate) {
+        console.error("TemplateName is not selected!");
+        snackbar.showSnackbar("Please select a template.", "error");
+        return;
+      }
+
       console.log("Create request body:", requestBody);
 
       let response = await postAsync(url, requestBody);
       console.log("Create response:", response);
 
-      //   onClose();
+      snackbar.showSnackbar("Settings Created", "success");
     } catch (error) {
       console.error("Error creating data:", error);
+      snackbar.showSnackbar("Error creating settings", "error");
     }
   };
 
@@ -110,20 +129,46 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
     try {
       let url = `${getSgBizBaseUrl()}/os-setting/update`;
 
-      let requestBody = { ...settings };
+      let updatedSettings = {
+        ...settings,
+        TemplateName: selectedTemplate || settings.TemplateName,
+      };
 
-      let response = await postAsync(url, requestBody);
+      console.log("----requestBody-----", JSON.stringify(updatedSettings)); // Add log to verify request body
+
+      let response = await postAsync(url, updatedSettings);
       snackbar.showSnackbar("Settings Updated", "success");
     } catch (error) {
       snackbar.showSnackbar("Could not Update Settings", "error");
     }
   };
 
+  const fetchTemplateData = async () => {
+    try {
+      const url = `${getSgBizBaseUrl()}/template/os/get/all`;
+      const response = await getAsync(url);
+
+      if (response?.Data) {
+        console.log("Template data fetched:", response.Data);
+        const values = response.Data.map((entry: any) => ({
+          name: entry.TemplateName,
+          ...entry,
+        }));
+
+        return values;
+      }
+      return [];
+    } catch (error) {
+      console.error("Error fetching template data: ", error);
+      return [];
+    }
+  };
+
   return (
-    <Box className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 overflow-y-scroll">
-      <div className="bg-white p-8 rounded shadow-md w-1/3 overflow-y-hidden">
-        <div className="flex flex-row justify-between items-center mb-4">
-          <Typography className="text-black text-xl">
+    <Box className="fixed  inset-0 flex items-center justify-center  bg-gray-900 bg-opacity-50  overflow-y-scroll">
+      <div className="bg-white p-7 rounded-2xl shadow-md w-11/12  max-w-lg max-h-screen overflow-y-scroll">
+        <div className="flex flex-row justify-between items-center mb-4 ml-4">
+          <Typography className="text-black text-xl mr-5">
             Configure Settings
           </Typography>
           <IconButton size={"large"} onClick={onClose}>
@@ -141,7 +186,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
               }}
               defaultValue={settings.CutOffDate}
             />
-            <div className="mt-2" />
+            <div className="mt-5 sm:mt-4" />
             <TextInput
               mode="number"
               placeHolder="Over-Due Days"
@@ -153,7 +198,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
               }}
               defaultValue={settings.OverDueDays.toString()}
             />
-            <div className="mt-2" />
+            <div className="mt-5 sm:mt-4" />
             <TextInput
               mode="number"
               placeHolder="Reminder Interval Days"
@@ -166,8 +211,30 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
               defaultValue={settings.ReminderIntervalDays.toString()}
             />
 
-            <div className="mt-4" />
+            <div className="mt-5 sm:mt-4" />
             <Button
+              sx={{
+                color: "white",
+                fontSize: { xs: "0.730rem", md: "0.9rem" },
+                fontWeight: "normal",
+                paddingX: 2,
+                paddingY: 1,
+                borderRadius: 3,
+                boxShadow: 4,
+                "&:hover": {
+                  boxShadow: 10,
+                },
+                "&:focus": {
+                  outline: "none",
+                  ring: 2,
+                },
+                transition: "box-shadow 0.2s",
+                textTransform: "capitalize",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
               variant={"contained"}
               onClick={() => {
                 toggleEmailConfig(!showEmailConfig);
@@ -178,7 +245,25 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
 
             {showEmailConfig && (
               <Box>
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
+                <ApiDropDown
+                  label="Select Template"
+                  displayFieldKey="name"
+                  valueFieldKey="name"
+                  onApi={fetchTemplateData}
+                  helperText=""
+                  defaultSelectionIndex={Number(settings.TemplateName)}
+                  onSelection={(selectedItem) => {
+                    console.log("Template selected:", selectedItem);
+                    setSelectedTemplate(selectedItem);
+                    setSettings((prevSettings) => ({
+                      ...prevSettings,
+                      TemplateName: selectedItem,
+                    }));
+                  }}
+                />
+
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="Mail Server"
@@ -190,7 +275,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.SmtpServer}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="Mail Port"
@@ -202,7 +287,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.SmtpPort}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="To Email(s)"
@@ -217,7 +302,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.To.join(",")}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="Cc Email(s)"
@@ -232,7 +317,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.Cc.join(",")}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="Bcc Email(s)"
@@ -247,7 +332,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.Bcc.join(",")}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   placeHolder="Subject"
@@ -259,7 +344,7 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
                   }}
                   defaultValue={settings.EmailSetting.Subject}
                 />
-                <div className="mt-2" />
+                <div className="mt-5 sm:mt-4" />
                 <TextInput
                   mode="text"
                   multiline={true}
@@ -275,19 +360,70 @@ const OsSettingsView = ({ onClose }: { onClose: () => void }) => {
               </Box>
             )}
 
-            <div className="mt-4" />
-            <Button variant={"contained"} onClick={handleUpdate}>
-              Update
-            </Button>
+            <div className="mt-5" />
+            <div className="mt-1 flex flex-row sm:flex-row justify-between gap-4">
+              <Button
+                variant={"contained"}
+                sx={{
+                  color: "white",
+                  fontSize: { xs: "0.687rem", md: "0.9rem" }, // Reduced font size for mobile
+                  fontWeight: "normal",
+                  paddingX: { xs: 1, md: 2 }, // Smaller padding for mobile
+                  paddingY: { xs: 0.8, md: 0.9 }, // Smaller padding for mobile
+                  borderRadius: { xs: 2.5, md: 3 }, // Equivalent to rounded-md
+                  boxShadow: 4, // Equivalent to shadow
+                  "&:hover": {
+                    boxShadow: 10, // Equivalent to hover:shadow-lg
+                  },
+                  "&:focus": {
+                    outline: "none",
+                    ring: 2,
+                    // Equivalent to focus:ring-2 focus:ring-gray-400
+                  },
+                  transition: "box-shadow 0.2s", // Smooth transition for the box shadow
+                  textTransform: "capitalize",
+                  width: "100%", // Full width
+                  display: "flex", // Use flexbox for alignment
+                  justifyContent: "center", // Center align items
+                  alignItems: "center",
+                }}
+                onClick={handleUpdate}
+              >
+                Update
+              </Button>
 
-            <Button
-              variant={"contained"}
-              onClick={() => {
-                router.push("/dashboard/settings/email-template");
-              }}
-            >
-              Create Email Template
-            </Button>
+              <Button
+                variant={"contained"}
+                sx={{
+                  color: "white",
+                  fontSize: { xs: "0.662rem", md: "0.9rem" }, // Reduced font size for mobile
+                  fontWeight: "normal",
+                  paddingX: { xs: 1, md: 2 }, // Smaller padding for mobile
+                  paddingY: { xs: 0.8, md: 0.9 }, // Smaller padding for mobile
+                  borderRadius: { xs: 2.5, md: 3 }, // Equivalent to rounded-md
+                  boxShadow: 4, // Equivalent to shadow
+                  "&:hover": {
+                    boxShadow: 10, // Equivalent to hover:shadow-lg
+                  },
+                  "&:focus": {
+                    outline: "none",
+                    ring: 2,
+                    // Equivalent to focus:ring-2 focus:ring-gray-400
+                  },
+                  transition: "box-shadow 0.2s", // Smooth transition for the box shadow
+                  textTransform: "capitalize",
+                  width: "100%", // Full width
+                  display: "flex", // Use flexbox for alignment
+                  justifyContent: "center", // Center align items
+                  alignItems: "center",
+                }}
+                onClick={() => {
+                  router.push("/dashboard/settings/email-template");
+                }}
+              >
+                Create Email Template
+              </Button>
+            </div>
           </>
         ) : (
           <Button variant={"contained"} onClick={handleCreate}>
