d7c79976ff0c2181f0d4c34ccbe03ac7bae553c5
softgen-mustafa <softgen.mustafa.khan@gmail.com>
2024-10-16 15:38:09 +0530

    commented the unwanted part

--- app/dashboard/drawer_config.tsx
+++ app/dashboard/drawer_config.tsx
@@ -1,14 +1,14 @@
 "use client";
 import {
-    Home,
-    PendingActions,
-    Receipt,
-    Settings,
-    Inventory,
+  Home,
+  PendingActions,
+  Receipt,
+  Settings,
+  Inventory,
 } from "@mui/icons-material";
 
 const customReportPaths: any[] = [
-    /*{
+  /*{
         title: "Broker Sales",
         destination: "/dashboard/broker/sales",
         icon: <Receipt />,
@@ -16,7 +16,7 @@ const customReportPaths: any[] = [
     },*/
 ];
 const memberReportPaths: any[] = [
-    /*
+  /*
     {
         title: "Broker Sales",
         destination: "/dashboard/broker/sales",
@@ -33,58 +33,54 @@ const memberReportPaths: any[] = [
 ];
 
 const adminReportPaths: any[] = [
-    {
-        title: "Home",
-        destination: "/dashboard",
-        icon: <Home />,
-        selected: true,
-    },
-    {
-        title: "Outstanding Report",
-        destination: "/dashboard/outstanding/report",
-        icon: <PendingActions />,
-        selected: false,
-    },
-    {
-        title: "Follow Up",
-        destination: "/dashboard/followup",
-        icon: <Receipt />,
-        selected: false,
-    },
-    {
-        title: "Stock Items",
-        destination: "/dashboard/Inventory/stock-items",
-        icon: <Inventory />,
-        selected: false,
-    },
-    {
-        title: "Transactions",
-        destination: "/dashboard/vouchers",
-        icon: <Receipt />,
-        selected: false,
-    },
-    {
-        title: "All Inventory",
-        destination: "/dashboard/Inventory",
-        icon: <Inventory />,
-        selected: false,
-    },
-    {
-        title: "Aging Outstanding",
-        destination: "/dashboard/outstanding",
-        icon: <PendingActions />,
-        selected: false,
-    },
-    {
-        title: "Settings",
-        destination: "/dashboard/settings",
-        icon: <Settings />,
-        selected: false,
-    },
+  {
+    title: "Home",
+    destination: "/dashboard",
+    icon: <Home />,
+    selected: true,
+  },
+  {
+    title: "Outstanding Report",
+    destination: "/dashboard/outstanding/report",
+    icon: <PendingActions />,
+    selected: false,
+  },
+  {
+    title: "Follow Up",
+    destination: "/dashboard/followup",
+    icon: <Receipt />,
+    selected: false,
+  },
+  {
+    title: "Stock Items",
+    destination: "/dashboard/Inventory/stock-items",
+    icon: <Inventory />,
+    selected: false,
+  },
+  {
+    title: "Transactions",
+    destination: "/dashboard/vouchers",
+    icon: <Receipt />,
+    selected: false,
+  },
+  // {
+  //     title: "All Inventory",
+  //     destination: "/dashboard/Inventory",
+  //     icon: <Inventory />,
+  //     selected: false,
+  // },
+  // {
+  //     title: "Aging Outstanding",
+  //     destination: "/dashboard/outstanding",
+  //     icon: <PendingActions />,
+  //     selected: false,
+  // },
+  {
+    title: "Settings",
+    destination: "/dashboard/settings",
+    icon: <Settings />,
+    selected: false,
+  },
 ];
 
-export {
-    customReportPaths,
-    memberReportPaths,
-    adminReportPaths
-}
+export { customReportPaths, memberReportPaths, adminReportPaths };
